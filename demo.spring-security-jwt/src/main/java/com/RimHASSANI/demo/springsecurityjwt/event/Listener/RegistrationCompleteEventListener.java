3a1a0a1ef4e249a8cdf1e7df24849485a8b58e03
HASSANI IDRISSI Rim <74383907+harim12@users.noreply.github.com>
2023-08-12 14:07:21 +0100

    sending email to verify

--- demo.spring-security-jwt/src/main/java/com/RimHASSANI/demo/springsecurityjwt/event/Listener/RegistrationCompleteEventListener.java
+++ demo.spring-security-jwt/src/main/java/com/RimHASSANI/demo/springsecurityjwt/event/Listener/RegistrationCompleteEventListener.java
@@ -4,6 +4,8 @@ import com.RimHASSANI.demo.springsecurityjwt.event.RegistrationCompleteEvent;
 import com.RimHASSANI.demo.springsecurityjwt.model.ApplicationUser;
 import com.RimHASSANI.demo.springsecurityjwt.service.AuthentificationUserService;
 import com.RimHASSANI.demo.springsecurityjwt.service.UserService;
+import com.RimHASSANI.demo.springsecurityjwt.utils.EmailUtil;
+import jakarta.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
@@ -19,20 +21,31 @@ public class RegistrationCompleteEventListener implements
     @Autowired
     private AuthentificationUserService userService;
 
+    @Autowired
+    private EmailUtil emailUtil;
+
     @Override
     public void onApplicationEvent(RegistrationCompleteEvent event) {
         //Create the Verification Token for the User with Link
         ApplicationUser user = event.getUser();
         String token = UUID.randomUUID().toString();
         userService.saveVerificationTokenForUser(token,user);
-        //Send Mail to user
+
         String url =
                 event.getApplicationUrl()
                         + "/verifyRegistration?token="
                         + token;
 
         //sendVerificationEmail()
+
+        try {
+            emailUtil.sendOtpEmail(user.getEmail(), token,url);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Unable to send otp please try again");
+        }
         log.info("Click the link to verify your account: {}",
                 url);
+
+
     }
 }
