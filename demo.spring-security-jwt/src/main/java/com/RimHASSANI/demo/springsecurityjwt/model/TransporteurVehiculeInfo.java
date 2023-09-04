a5a158e3015a9273db792ab3a5f4f1f49eef8ee8
HASSANI IDRISSI Rim <74383907+harim12@users.noreply.github.com>
2023-09-04 12:12:06 +0100

    adding entity transporteur vehicule info

---
+++ demo.spring-security-jwt/src/main/java/com/RimHASSANI/demo/springsecurityjwt/model/TransporteurVehiculeInfo.java
@@ -0,0 +1,19 @@
+package com.RimHASSANI.demo.springsecurityjwt.model;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class TransporteurVehiculeInfo {
+
+    private String imageVehiculeUrl;
+
+    private String driverLiscence;
+    private String nationalIdentity;
+    private String vehiculeRegistrationNumber;
+
+}
